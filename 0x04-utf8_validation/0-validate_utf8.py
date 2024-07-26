2f8717e4c44693ad6d71385c202ce634fc87192c
YOUNESSE25 <younessehaouas@gmail.com>
2024-07-26 03:59:30 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-0. UTF-8 Validation
+UTF-8 Validation
 """
 
 
 def validUTF8(data):
     """
-    method that determines if a given data set represents
-    a valid UTF-8 encoding. 
+    Data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
     """
     byte_count = 0
 
