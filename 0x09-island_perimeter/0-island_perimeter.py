b8083572646751a1238fb123ecd62738d1cd3ffe
YOUNESSE25 <younessehaouas@gmail.com>
2024-08-30 02:03:42 +0100

    island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,62 +1,62 @@
 #!/usr/bin/python3
-'''IsLand Perimeter'''
+'''0x09. Island Perimeter'''
 
 
-def isLand_perimeter(grid):
-    '''IsLand Perimeter function'''
-    conteur = 0
-    grd_max = len(grid) - 1  # last list in the grid
-    lst_max = len(grid[0]) - 1  # last square in list
+def island_perimeter(grid):
+    '''returns the perimeter of the island described in grid'''
+    counter = 0
+    grid_max = len(grid) - 1  # index of the last list in the grid
+    lst_max = len(grid[0]) - 1  # index of the last square in list
 
     for lst_idx, lst in enumerate(grid):
-        for Land_idx, Land in enumerate(lst):
-            if Land == 1:
-                # right and left
-                if Land_idx == 0:
-                    # left
-                    conteur += 1
+        for land_idx, land in enumerate(lst):
+            if land == 1:
+                # left and right
+                if land_idx == 0:
+                    # left side
+                    counter += 1
 
-                    # right
-                    if lst[Land_idx + 1] == 0:
-                        conteur += 1
-                elif Land_idx == lst_max:
-                    # left
-                    if lst[Land_idx - 1] == 0:
-                        conteur += 1
+                    # right side
+                    if lst[land_idx + 1] == 0:
+                        counter += 1
+                elif land_idx == lst_max:
+                    # left side
+                    if lst[land_idx - 1] == 0:
+                        counter += 1
 
-                    # right
-                    conteur += 1
+                    # right side
+                    counter += 1
                 else:
-                    # left
-                    if lst[Land_idx - 1] == 0:
-                        conteur += 1
+                    # left side
+                    if lst[land_idx - 1] == 0:
+                        counter += 1
 
-                    # right
-                    if lst[Land_idx + 1] == 0:
-                        conteur += 1
+                    # right side
+                    if lst[land_idx + 1] == 0:
+                        counter += 1
 
-                # down and top
+                # top and down
                 if lst_idx == 0:
-                    # top
-                    conteur += 1
+                    # top side
+                    counter += 1
 
-                    # bottom
-                    if grid[lst_idx + 1][Land_idx] == 0:
-                        conteur += 1
-                elif lst_idx == grd_max:
-                    # top
-                    if grid[lst_idx - 1][Land_idx] == 0:
-                        conteur += 1
+                    # bottom side
+                    if grid[lst_idx + 1][land_idx] == 0:
+                        counter += 1
+                elif lst_idx == grid_max:
+                    # top side
+                    if grid[lst_idx - 1][land_idx] == 0:
+                        counter += 1
 
-                    # bottom
-                    conteur += 1
+                    # bottom side
+                    counter += 1
                 else:
-                    # top
-                    if grid[lst_idx - 1][Land_idx] == 0:
-                        conteur += 1
+                    # top side
+                    if grid[lst_idx - 1][land_idx] == 0:
+                        counter += 1
 
-                    # bottom
-                    if grid[lst_idx + 1][Land_idx] == 0:
-                        conteur += 1
+                    # bottom side
+                    if grid[lst_idx + 1][land_idx] == 0:
+                        counter += 1
 
-    return conteur
+    return counter
--- 0x09-island_perimeter/README.md
+++ 0x09-island_perimeter/README.md
@@ -1,4 +1,4 @@
-[# 0x09. Island Perimeter](0-island_perimeter.py)
+# 0x09. Island Perimeter
 
 Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
 
